0e01f76d58126b435d2139905eb9b2193da9dc8d
miew-miew <nrzanakoto011@gmail.com>
2024-02-08 10:51:17 +0100

    routing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,23 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('/blog')->name('blog.')->group(function () {
+    Route::get('/', function (Request $request) {
+        return [
+            "link" => \route('blog.show', ["slug" => "article", "id" => 13])
+        ];
+    })->name('index');
+
+    Route::get('/{slug}-{id}', function (string $slug, string $id, Request $request) {
+        return [
+            "slug" => $slug,
+            "id" => $id,
+            "name" => $request->input('name', 'Ny Aina')
+        ];
+    })->where([
+        "id" => '[0-9]+',
+        "slug" => '[a-z0-9\-]+'
+    ])->name('show');
 });
