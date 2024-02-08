097850f9a7cb40245d0928685db6ec8f1ea26fc0
miew-miew <nrzanakoto011@gmail.com>
2024-02-08 11:47:16 +0100

    views

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -6,20 +6,25 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Contracts\Pagination\Paginator;
+use Illuminate\View\View;
 
 class BlogController extends Controller
 {
-    public function index(): Paginator
+    public function index(): View
     {
-        return Post::paginate(25);
+        return view('blog.index', [
+            'posts' => Post::paginate(1)
+        ]);
     }
 
-    public function show(string $slug, string $id): RedirectResponse | Post
+    public function show(string $slug, string $id): RedirectResponse | View
     {
         $post = Post::findOrFail($id);
         if($post->slug !== $slug){
             return to_route('blog.show', ["slug" => $post->slug, "id" => $post->id]);
         }
-        return $post;
+        return view('blog.show', [
+            'post' => $post
+        ]);
     }
 }
---
+++ resources/views/base.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+
+@php
+$routeName = request()->route()->getName();
+@endphp
+    <!-- Barre de navigation Bootstrap -->
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <div class="container-fluid">
+            <!-- Bouton de bascule pour les petits écrans -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- Liens de navigation -->
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                    <li class="nav-item">
+                        <a @class(['nav-link', 'active' => str_starts_with($routeName, 'blog.')]) aria-current="page" href="{{route('blog.index')}}">Accueil</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
